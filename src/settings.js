07b689fa34ede37e29a2bfaae4fc3681058240dd
YimingLu_cnjs <565948604@qq.com>
2019-10-21 10:09:48 +0800

    ico修改，项目名称修改

--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: 'Vue Element Admin',
+  title: '餐饮油烟监测管理系统',
 
   /**
    * @type {boolean} true | false
--- src/utils/get-page-title.js
+++ src/utils/get-page-title.js
@@ -1,6 +1,6 @@
 import defaultSettings from '@/settings'
 
-const title = defaultSettings.title || 'Vue Element Admin'
+const title = defaultSettings.title || '餐饮油烟监测管理系统'
 
 export default function getPageTitle(pageTitle) {
   if (pageTitle) {
