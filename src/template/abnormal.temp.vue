19ed8913504afd06d812a06ef59110ffc14e4cdf
YimingLu_cnjs <565948604@qq.com>
2019-10-30 10:06:47 +0800

    暂时忽略eslint提交

--- src/template/abnormal.temp.vue
+++ src/template/abnormal.temp.vue
@@ -1,24 +1,31 @@
 <template>
-  <el-container style="height: 500px; padding: 15px; border: 1px solid #eee">
-    <el-aside width="200px" style="background-color: rgb(238, 241, 246)">1</el-aside>
-    <el-container>
-      <el-header style="text-align: right; font-size: 12px">1</el-header>
-      <el-main>1</el-main>
-    </el-container>
-  </el-container>
+	<el-container class="kd-abnormal-template">
+		<el-aside width="300px" class="kd-left-tree-wrap" ></el-aside>
+		<el-container>
+			<el-header style="text-align: right; font-size: 12px">1</el-header>
+			<el-main>1</el-main>
+		</el-container>
+	</el-container>
 </template>
+
 <script>
-export default {
-  name: 'KdAbnormalTemplate',
-  data() {
-    return {
+	export default {
+		name: 'KdAbnormalTemplate',
+		data() {
+			return {
 
-    }
-  }
-}
+			}
+		}
+	}
 </script>
-<style lang="scss" scoped>
-.kd-abnormal-template{
 
-}
+<style lang="scss" scoped>
+	.kd-abnormal-template{
+		height: calc(100vh - 85px); 
+		padding: 15px; 
+		border: 1px solid #eee;
+		.kd-left-tree-wrap{
+
+		}
+	}
 </style>
