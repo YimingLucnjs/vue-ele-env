9df016a11e2b43fd4e903cb427e40c633aae7a52
qiuliang1 <985444288@qq.com>
2019-12-18 20:14:45 +0800

    表格组件添加

--- src/views/testcom/qiuliang.vue
+++ src/views/testcom/qiuliang.vue
@@ -14,7 +14,7 @@
       <el-row>
         <el-col
           class="grid-content bg-purple"
-          style="height:calc(100vh - 314px)"
+          style="height:calc(100vh - 314px);position:relative"
           :span="24"
         >
           <count
@@ -32,6 +32,18 @@
             width="40%"
           />
         </el-col>
+        <el-col
+          :span="24"
+          style="height:calc(100vh - 707px);background:#032449"
+        >
+          <div class="table_norder">
+            <info-table
+              :table-data="tableData"
+              :table-head="tableHead"
+              height="100%"
+            />
+          </div>
+        </el-col>
       </el-row>
     </el-col>
     <el-col
@@ -45,9 +57,78 @@
 import mapChart from "@/components/ql_component/Charts/mapChart";
 import barChart from "@/components/ql_component/Charts/barChart";
 import Count from "@/components/ql_component/Count";
+import infoTable from "@/components/ql_component/Table/info-table";
 export default {
   components: {
-    mapChart,barChart,Count
+    mapChart,barChart,Count,infoTable
+  },
+  data() {
+    return {
+      tableData: [
+        {
+          enterprise: '中石油集团测井有限公司华北事业部',
+          sourceCode: '0193AB099522',
+          nucleusName: '某某某',
+          pollution: 'III',
+          dose: '8.10',
+          state: '当前状态'
+        },
+        {
+          enterprise: '中石油集团测井有限公司华北事业部',
+          sourceCode: '0193AB099522',
+          nucleusName: '某某某',
+          pollution: 'III',
+          dose: '8.10',
+          state: '当前状态'
+        },
+        {
+          enterprise: '中石油集团测井有限公司华北事业部',
+          sourceCode: '0193AB099522',
+          nucleusName: '某某某',
+          pollution: 'III',
+          dose: '8.10',
+          state: '当前状态'
+        }
+      ],
+      tableHead: [
+        {
+          prop: "enterprise",
+          label: "企业",
+          width: "",
+          align: "center"
+        },
+        {
+          prop: "sourceCode",
+          label: "源编码",
+          width: "",
+          align: "center"
+        },
+        {
+          prop: "nucleusName",
+          label: "核素名称",
+          width: "",
+          align: "center"
+        },
+        {
+          prop: "pollution",
+          label: "污染程度",
+          width: "",
+          align: "center"
+        },
+        {
+          prop: "dose",
+          label: "剂量（uSv/h）",
+          width: "",
+          align: "center"
+        },
+        {
+          prop: "state",
+          label: "当前状态",
+          width: "",
+          align: "center"
+        }
+      ]
+    }
   }
 }
 </script>
@@ -58,5 +139,11 @@ export default {
   }
   .bg-purple {
     background: #d3dce6;
+  }
+  .table_norder {
+    padding: 10px;
+    height: 100%;
+    border-radius: 5px;
+    border: 1px solid #1403a8;
   }
 </style>
